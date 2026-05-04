ea1ce71fe0ca56c2e8e0db6094ef70ebecc2584f
vimukthica94-maker <>
2026-05-04 01:11:27 +0000

    Membership and Types

--- conditional_statements/membership_and_types/main.py
+++ conditional_statements/membership_and_types/main.py
@@ -3,8 +3,10 @@ description = "Imported honey, raw and unfiltered"
 price = "5.99"
 count = 120
 
-# Write your code here
-
+contains_raw = "raw" in description
+contains_Imported = "Imported" in description
+price_is_float = type(price) == float 
+count_is_int = type(count) == int
 
 
 print("Contains 'raw':", contains_raw)
