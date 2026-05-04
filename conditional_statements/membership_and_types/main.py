48d000398b7696072de4b11d3902e92ef690cd7c
vimukthica94-maker <>
2026-05-04 01:05:02 +0000

    Membership and Types

---
+++ conditional_statements/membership_and_types/main.py
@@ -0,0 +1,13 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+
+# Write your code here
+
+
+
+print("Contains 'raw':", contains_raw)
+print("Contains 'Imported':", contains_Imported)
+print("Is price a float?:", price_is_float)
+print("Is count an integer?:", count_is_int)
